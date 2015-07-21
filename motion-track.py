beb36eb56e498f4a6d11814b03d6c8b9b5d5783b
Claude Pageau <pageauc@gmail.com>
2015-07-21 09:43:52 -0400

    Added show_FPS function
    display FPS for last 10 frames if debug on

--- motion-track.py
+++ motion-track.py
@@ -34,14 +34,14 @@ import picamera.array
 import cv2
 import numpy as np
 
-debug = True      # Set to False for no data display
+debug = True       # Set to False for no data display
 window_on = True  # Set to True displays opencv windows (GUI desktop reqd)
 
 # Camera Settings
 CAMERA_WIDTH = 320
 CAMERA_HEIGHT = 240
 CAMERA_HFLIP = True
-CAMERA_VFLIP = False
+CAMERA_VFLIP = True
 
 # Motion Tracking Settings
 THRESHOLD_SENSITIVITY = 25
@@ -49,8 +49,20 @@ BLUR_SIZE = 10
 MIN_AREA = 25     # excludes all contours less than or equal to this Area
 CIRCLE_SIZE = 10  # diameter of circle to show motion location in window
 
+def show_FPS(start_time,frame_count):
+    if debug:
+        if frame_count >= 10:
+            duration = float(time.time() - start_time)
+            FPS = float(frame_count / duration)
+            print("Processing at %.2f FPS last %i frames" %( FPS, frame_count))
+            frame_count = 0
+            start_time = time.time()
+        else:
+            frame_count += 1
+    return start_time, frame_count
+
 def motion_track():
-    print("Initializing Camera ...")
+    print("Initializing Camera ....")
     # Save images to an in-program stream
     stream = io.BytesIO()
     with picamera.PiCamera() as camera:
@@ -59,12 +71,15 @@ def motion_track():
         camera.vflip = CAMERA_VFLIP      
         time.sleep(2)
         first_image = True
-        print("Start Motion Tracking ....")
         if window_on:
             print("press q to quit opencv display")
         else:
             print("press ctrl-c to quit")        
+        print("Start Motion Tracking ....")
+        frame_count = 0
+        start_time = time.time()
         while(True):
+            start_time, frame_count = show_FPS(start_time, frame_count)
             # initialize variables         
             motion_found = False
             biggest_area = MIN_AREA
@@ -114,9 +129,6 @@ def motion_track():
                             cv2.circle(image2,(cx,cy),CIRCLE_SIZE,(0,255,0),2)
                         if debug:
                             print("total_Contours=%2i  Motion at cx=%3i cy=%3i   biggest_area:%3ix%3i=%5i" % (total_contours, cx ,cy, cw, ch, biggest_area))
-                    else:
-                        if debug:                
-                            print("total_contours=%2i  No Motion" % (total_contours))                    
                     if window_on:
                         # cv2.imshow('Difference Image',differenceimage) 
                         cv2.imshow('Threshold Image', thresholdimage)
