7b63b69592be5cf2b8bf11090edaa054747125c9
Claude Pageau <pageauc@gmail.com>
2016-07-03 15:59:25 -0400

    fixed bug in script for install.sh

--- speed-track-2/install.sh
+++ speed-track-2/install.sh
@@ -13,7 +13,7 @@ echo "3 - Creating folder speed2"
 mkdir speed2
 cd speed2
 echo "4 - Downloading github repo files"
-wget -O speed2.py https://raw.github.com/pageauc/motion-track/master/speed-track-2/install.sh
+wget -O install.sh https://raw.github.com/pageauc/motion-track/master/speed-track-2/install.sh
 wget -O speed2.py https://raw.github.com/pageauc/motion-track/master/speed-track-2/speed2.py
 wget -O speed2.sh https://raw.github.com/pageauc/motion-track/master/speed-track-2/speed2.sh
 wget -O Readme.md https://raw.github.com/pageauc/motion-track/master/speed-track-2/Readme.md
