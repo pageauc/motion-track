6cc0ade0a07fa97b2414e6958dce78c984f01980
Claude Pageau <pageauc@gmail.com>
2016-07-04 08:58:28 -0400

    install.sh Formatting changes

--- speed-track-2/install.sh
+++ speed-track-2/install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 # Convenient speed2 install.sh script written by Claude Pageau 1-Jul-2016
-# ver 1.5
-echo "speed2 install/update"
-echo "1 - Performing System Updates"
+ver="1.6"
+echo "      speed2 Install.sh script ver $ver"
+echo "Install or Upgrade speed2 Object speed tracking"
+echo "-----------------------------------------------"
+echo "1 - Performing Raspbian System Update"
 sudo apt-get -y update
-echo "1 - Performing System Upgrade"
+echo "1 - Performing Raspbian System Upgrade"
 sudo apt-get -y upgrade
-echo "2 - Installing Dependencies"
+echo "2 - Installing speed2 Dependencies"
 sudo apt-get install -y python-opencv python-picamera python-imaging python-pyexiv2 libgl1-mesa-dri
 sudo apt-get install -y fonts-freefont-ttf # Required for Jessie Lite Only
 cd ~
-echo "3 - Creating folder speed2"
+echo "3 - Create folder speed2"
 mkdir -p speed2
 cd speed2
 echo "4 - Downloading github repo files"
@@ -24,9 +26,17 @@ chmod +x speed2.py
 chmod +x speed2.sh
 chmod +x install.sh
 echo "6 - Installation Complete"
+echo "-----------------------------------------------"
 echo "See Readme.md for program details"
+echo
 echo "Note: if config.py already exists then new file ends with a sequence number"
-echo "You should reboot RPI if there are significant system file updates"
+echo "You should reboot RPI if there are significant Raspbian system file updates"
+echo "Check speed2 settings in config.py file using nano and customize as required"
+echo "You must perform a speed2 calibration procedure per Readme.md file instructions"
+echo "To start speed2 perform the following while in the speed2 folder"
+echo "./speed2.py"
+echo
+echo "Good Luck Claude" 
 
 
 
