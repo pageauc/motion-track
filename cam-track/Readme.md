baeaf8ce0d853e2c495d4366c38b2670e5968a20
Claude Pageau <pageauc@gmail.com>
2016-08-16 06:35:27 -0400

    .07 Release - comments and a few updates
    moved np import and changed video framerate for slower RPI's

--- cam-track/Readme.md
+++ cam-track/Readme.md
@@ -1,24 +1,27 @@
-# cam-track.py
-####  A Raspberry Pi opencv2 camera movement tracking using Video Streaming and Threading
+# cam-track.py  Camera Movement Tracker
+#### A Raspberry Pi openCV2 Camera Movement Tracker using a Video Stream Thread
 
 ###Release History
 * ver 0.6 15-Aug-2016 - Initial Release
+* ver 0.7 16-Aut-2016 - Added extra comments and move np import
 
 ### Program Description
-This is a raspberry pi computer openCV2 progrm that tracks camera (pan/tilt)
+This is a raspberry pi computer openCV2 program that tracks camera (pan/tilt)
  movements. It requires a RPI camera module installed and working. The program is 
-written in python2 and uses openCV2.  It capture a search rectangle
-from the center of the threaded video stream. It then locates the rectangle in 
-subsequent images based on a score value and returns the x y location in
-the image.  If movement gets too close to the sides of the image or
-a suitable image search match cannot be found, then another search rectangle
-is selected. This data is processed to track a cumulative pixel location base on
-an initial camera image center value of 0,0.    
-This code could be used for a simple robotics application or where the camera is 
-mounted on a moving platform or object.  
+written in python2 and uses openCV2.  
 
-This application is a demo and is currently still in development but I 
-thought it could still be useful since I was not able to find a similar
+It captures a search rectangle from the center of a video stream tread. 
+It then locates the rectangle in subsequent images based on a score value and
+returns the x y location in the image based on a threshold accuracy.  
+If movement gets too close to the sides of the image or
+a suitable image search match cannot be found, then another search rectangle
+is selected. This data is processed to track a cumulative pixel location based on
+an initial camera image center value of 0,0.    
+This code could be used for a simple robotics application, movement stabilization, 
+where the camera is mounted on a moving platform or object, Etc.  
+
+Note: This application is a demo and is currently still in development but I 
+thought it could still be useful, since I was not able to find a similar
 RPI application that does this.
 
 Thanks to Adrian Rosebrock jrosebr1 at http://www.pyimagesearch.com 
@@ -42,7 +45,8 @@ From logged in RPI SSH session or console terminal perform the following.
     wget https://raw.github.com/pageauc/motion-track/master/cam-track/cam-track.py
     chmod +x cam-track.py  
     ./cam-track.py       # defaults to run from RPI desktop terminal session
-  
+                         # Set window_on=False if running in SSH session
+                         
 #### Tuning
 You may have to experiment with some settings to optimize performance.
 If there are plain backgrounds or random motions in camera view then the
--- cam-track/cam-track.py
+++ cam-track/cam-track.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 progname = "cam-track.py"
-ver = "version 0.6"
+ver = "version 0.7"
 
 """
-cam-track ver 0.6 written by Claude Pageau pageauc@gmail.com
+cam-track ver 0.7 written by Claude Pageau pageauc@gmail.com
 Raspberry (Pi) - python opencv2 camera pan/tilt tracking using picamera module
 
 This is a raspberry pi python opencv2 camera tracking demonstration program.
@@ -39,8 +39,9 @@ Good Luck  Claude ...
 
 """
 print("%s %s using python2 and OpenCV2" % (progname, ver))
-print("Track camera pan/tilt movements optically using openCV2")
+print("Camera movement (pan/tilt) Tracker using openCV2 image searching")
 print("Loading Please Wait ....")
+
 # import the necessary packages
 import io
 import time
@@ -50,6 +51,10 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 from threading import Thread
 from operator import itemgetter
+import numpy as np
+
+#-----------------------------------------------------------------------------------------------  
+# Global Variable Settings
 
 debug = True      # Set to False for no data display
 window_on = True   # Set to True displays opencv windows (GUI desktop reqd)
@@ -65,10 +70,11 @@ CAMERA_HEIGHT = 240
 CAMERA_HFLIP = False
 CAMERA_VFLIP = True
 CAMERA_ROTATION=0
-CAMERA_FRAMERATE = 100
+CAMERA_FRAMERATE = 35
 FRAME_COUNTER = 1000
 
 #-----------------------------------------------------------------------------------------------  
+# Create a Video Stream Tread
 class PiVideoStream:
     def __init__(self, resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=CAMERA_FRAMERATE, rotation=0, hflip=False, vflip=False):
         # initialize the camera and stream
@@ -119,6 +125,7 @@ class PiVideoStream:
         self.stopped = True
 
 #-----------------------------------------------------------------------------------------------  
+# Currently not used but included in case you want to check speed
 def show_FPS(start_time,frame_count):
     if debug:
         if frame_count >= FRAME_COUNTER:
@@ -137,25 +144,32 @@ def get_center(x,y,w,h):
     
 #-----------------------------------------------------------------------------------------------  
 def cam_shift():
-    import numpy as np
+    # Process steam images to find camera movement 
+    # using an extracted search rectangle in the middle of one frame
+    # and find location in subsequent images.  Grap a new search rect
+    # as needed based on nearness to edge of image or percent probability
+    # of image search result Etc.
+    
+    # Setup Video Stream Thread
     vs = PiVideoStream().start()
     vs.camera.rotation = CAMERA_ROTATION
     vs.camera.hflip = CAMERA_HFLIP
     vs.camera.vflip = CAMERA_VFLIP
     time.sleep(2.0)    
       
+    # initialize the search window (rect) variables  
     sw_w = int(CAMERA_WIDTH/4)    # search window width
     sw_h = int(CAMERA_HEIGHT/4)   # search window height
-    sw_buf_x = int(sw_w/4)    # buffer to left/right of image
-    sw_buf_y = int(sw_h/4)    # buffer to top/bot of image
-    sw_cx = int(CAMERA_WIDTH/2)  # x center of image
-    sw_cy = int(CAMERA_HEIGHT/2) # y center of image   
-    sw_x = (sw_cx - sw_w/2)  # top x corner of rect
-    sw_y = (sw_cy - sw_h/2)  # top y corner of rect
-    sw_maxLoc = .94    # Accuracy of search in image
-    sw_minVal = .45    # Lowest accuracy of search in image
+    sw_buf_x = int(sw_w/4)        # buffer to left/right of image
+    sw_buf_y = int(sw_h/4)        # buffer to top/bot of image
+    sw_cx = int(CAMERA_WIDTH/2)   # x center of image
+    sw_cy = int(CAMERA_HEIGHT/2)  # y center of image   
+    sw_x = (sw_cx - sw_w/2)       # top x corner of search rect
+    sw_y = (sw_cy - sw_h/2)       # top y corner of search rect
+    sw_maxLoc = .94               # Threshold Accuracy of search in image
+    sw_minVal = .45               # Threshold of worst search result in image
     
-    # take first frame of the video and initialize search rect
+    # Grab a Video Steam image and initialize search rectangle
     cam_cx1 = sw_cx
     cam_cy1 = sw_cy
     cam_cur_cx = cam_cx1
@@ -165,20 +179,27 @@ def cam_shift():
     cam_track_cx = 0   # initialize cam horizontal cam movement tracker
     cam_track_cy = 0   # initialize cam vertical cam movement tracker
     while True:
-        image1 = vs.read()
+        image1 = vs.read()  # capture a image from video stream thread
+        # Look for search_rect in this image and return result
         result = cv2.matchTemplate( image1, search_rect, cv2.TM_CCORR_NORMED)
+        # Process result to return probabilities and Location of best and worst image match
         minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)  # find search rect match in new image
-        cam_cx2, cam_cy2 = get_center( maxLoc[0], maxLoc[1], sw_w, sw_h )   
+        # Get the center of the best matching result of search
+        cam_cx2, cam_cy2 = get_center( maxLoc[0], maxLoc[1], sw_w, sw_h ) 
+        # Update cumulative camera tracking data        
         cam_track_cx = cam_track_cx + cam_cur_cx - cam_cx2
         cam_track_cy = cam_track_cy + cam_cur_cy - cam_cy2 
+        # Check if search rect is near edges and meets search accuracy threshold
         if not ( maxLoc[0] > sw_buf_x and maxLoc[0] + sw_x + sw_buf_x < CAMERA_WIDTH and
-             maxLoc[1] > sw_buf_y and maxLoc[1] + sw_y + sw_buf_y < CAMERA_HEIGHT
-             and maxVal > sw_maxLoc):
+                 maxLoc[1] > sw_buf_y and maxLoc[1] + sw_y + sw_buf_y < CAMERA_HEIGHT
+                 and maxVal > sw_maxLoc):
+            # check value of lowest matching result and reset search rectangle
             if minVal < sw_minVal:             
                 if debug:
                     print("    Reset search_rect cur_cx=%i cam_track_cx=%i cur_cy=%i cam_track_cy=%i" 
                     % (cam_cur_cx, cam_track_cx, cam_cur_cy, cam_track_cy))        
                 search_rect = image1[sw_y:sw_y+sw_h, sw_x:sw_x+sw_w]
+                
                 cam_cx2, cam_cy2 = get_center( maxLoc[0], maxLoc[1], sw_w, sw_h )   
                 cam_track_cx = cam_track_cx + cam_cur_cx - cam_cx2
                 cam_track_cy = cam_track_cy + cam_cur_cy - cam_cy2 
@@ -195,6 +216,7 @@ def cam_shift():
         image2 = image1
            
         if window_on:
+            # Display openCV window information on RPI desktop if required
             cv2.imshow( 'search rectangle', search_rect ) 
             #  cv2.circle(image2,(sw_x+sw_w/2,sw_y+sw_h/2),CIRCLE_SIZE,(0,0,255), 2) 
             #  cv2.rectangle(image2,(sw_x,sw_y),(sw_x+sw_w,sw_y+sw_h),(0,0,255), LINE_THICKNESS)         
@@ -210,7 +232,7 @@ def cam_shift():
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
-                print("End Cam Shift Tracking")
+                print("End Cam Tracking")
                 break
      
 #-----------------------------------------------------------------------------------------------    
