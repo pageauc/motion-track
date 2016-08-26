9a74e0dde33fc304fe350f05e3f299618d20ba8c
Claude Pageau <pageauc@gmail.com>
2016-08-26 07:47:14 -0400

    ver .72  Revised logic and bug fix

--- cam-track/cam-track.py
+++ cam-track/cam-track.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 progname = "cam-track.py"
-ver = "version 0.71"
+ver = "version 0.72"
 
 """
-cam-track ver 0.71 written by Claude Pageau pageauc@gmail.com
+cam-track ver 0.72 written by Claude Pageau pageauc@gmail.com
 Raspberry (Pi) - python opencv2 camera pan/tilt tracking using picamera module
 
 This is a raspberry pi python opencv2 camera tracking demonstration program.
@@ -60,7 +60,7 @@ fps_on = False        # Display fps (not implemented)
 
 # OpenCV Settings
 WINDOW_BIGGER = 2.0   # increase the display window size
-MAX_SEARCH_THRESHOLD = .97  # default=.97 Accuracy for best search result of search_rect in stream images
+MAX_SEARCH_THRESHOLD = .95  # default=.97 Accuracy for best search result of search_rect in stream images
 MIN_SEARCH_THRESHOLD = .45  # default=.45 Accuracy for worst search result of search rect in stream images
 LINE_THICKNESS = 1    # thickness of bounding line in pixels
 CV_FONT_SIZE = .25    # size of font on opencv window default .5
@@ -159,11 +159,12 @@ def cam_shift():
     vs.camera.hflip = CAMERA_HFLIP
     vs.camera.vflip = CAMERA_VFLIP
     time.sleep(2.0)    
-      
-    # initialize the search window (rect) variables 
+
     if WINDOW_BIGGER > 1:  # Note setting a bigger window will slow the FPS
         big_w = int(CAMERA_WIDTH * WINDOW_BIGGER)
         big_h = int(CAMERA_HEIGHT * WINDOW_BIGGER) 
+    
+    # initialize the search window (rect) variables 
     sw_w = int(CAMERA_WIDTH/4)    # search window width
     sw_h = int(CAMERA_HEIGHT/4)   # search window height
     sw_buf_x = int(sw_w/4)        # buffer to left/right of image
@@ -176,46 +177,47 @@ def cam_shift():
     sw_minVal = MIN_SEARCH_THRESHOLD  # Threshold of worst search result in image
     
     # Grab a Video Steam image and initialize search rectangle
-    cam_cx1 = sw_cx
-    cam_cy1 = sw_cy
-    cam_cur_cx = cam_cx1
+    cam_cx1 = sw_cx    # Set Cam x center start position
+    cam_cy1 = sw_cy    # Set Cam y center start position
+    cam_cur_cx = cam_cx1 
     cam_cur_cy = cam_cy1
+    cam_track_cx = 0   # initialize cam horizontal movement tracker
+    cam_track_cy = 0   # initialize cam vertical movement tracker
     image1 = vs.read()   # initialize first image
     search_rect = image1[sw_y:sw_y+sw_h, sw_x:sw_x+sw_w]  # Initialize centre search rectangle
-    cam_track_cx = 0   # initialize cam horizontal cam movement tracker
-    cam_track_cy = 0   # initialize cam vertical cam movement tracker
     while True:
-        image1 = vs.read()  # capture a image from video stream thread
+        image1 = vs.read()  # capture a new image1 from video stream thread
         # Look for search_rect in this image and return result
         result = cv2.matchTemplate( image1, search_rect, cv2.TM_CCORR_NORMED)
         # Process result to return probabilities and Location of best and worst image match
         minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)  # find search rect match in new image
         # Get the center of the best matching result of search
         cam_cx2, cam_cy2 = get_center( maxLoc[0], maxLoc[1], sw_w, sw_h ) 
-        # Update cumulative camera tracking data        
-        cam_track_cx = cam_track_cx + cam_cur_cx - cam_cx2
-        cam_track_cy = cam_track_cy + cam_cur_cy - cam_cy2 
-        # Check if search rect is near edges and meets search accuracy threshold
-        if not ( maxLoc[0] > sw_buf_x and maxLoc[0] + sw_x + sw_buf_x < CAMERA_WIDTH and
-                 maxLoc[1] > sw_buf_y and maxLoc[1] + sw_y + sw_buf_y < CAMERA_HEIGHT
-                 and maxVal > sw_maxVal):
-            # check value of lowest matching result and reset search rectangle
-            if minVal < sw_minVal:             
-                if debug:
-                    print("    Reset search_rect cur_cx=%i cam_track_cx=%i cur_cy=%i cam_track_cy=%i" 
-                    % (cam_cur_cx, cam_track_cx, cam_cur_cy, cam_track_cy))        
-                search_rect = image1[sw_y:sw_y+sw_h, sw_x:sw_x+sw_w]
-                
-                cam_cx2, cam_cy2 = get_center( maxLoc[0], maxLoc[1], sw_w, sw_h )   
-                cam_track_cx = cam_track_cx + cam_cur_cx - cam_cx2
-                cam_track_cy = cam_track_cy + cam_cur_cy - cam_cy2 
-                cam_cx1 = sw_cx
-                cam_cy1 = sw_cy
-                cam_cx2 = cam_cx1
-                cam_cy2 = cam_cy1
-        cam_cur_cx = cam_cx2
-        cam_cur_cy = cam_cy2 
-        
+        # Update cumulative camera tracking data
+        if cam_cx2 <> cam_cx1:        
+            cam_track_cx = cam_track_cx + (cam_cur_cx - cam_cx2)
+            cam_cur_cx = cam_cx2
+        if cam_cy2 <> cam_cy1:
+            cam_track_cy = cam_track_cy + (cam_cur_cy - cam_cy2)
+            cam_cur_cy = cam_cy2       
+        # Check if search rect is well inside image1
+        # and maxVal and minVal are above threshold
+        if ( maxLoc[0] < sw_buf_x 
+          or maxLoc[0] > CAMERA_WIDTH - (sw_buf_x + sw_w)
+          or maxLoc[1] < sw_buf_y 
+          or maxLoc[1] > CAMERA_HEIGHT - (sw_buf_y + sw_h)
+          or maxVal < sw_maxVal):                           
+            if debug:
+                print("    Reset search_rect cur_cx=%i cam_track_cx=%i cur_cy=%i cam_track_cy=%i" 
+                % (cam_cur_cx, cam_track_cx, cam_cur_cy, cam_track_cy))        
+            search_rect = image1[sw_y:sw_y+sw_h, sw_x:sw_x+sw_w]               
+            cam_cx1 = sw_cx
+            cam_cy1 = sw_cy    
+            cam_cx2 = cam_cx1         
+            cam_cy2 = cam_cy1
+            cam_cur_cx = cam_cx2                   
+            cam_cur_cy = cam_cy2             
+
         if debug: 
             print(" Cam at (%i,%i) cam_track_cx, cam_track_cy" % ( cam_track_cx, cam_track_cy, ))
             print(" maxLoc   maxVal   minLoc   minVal")
