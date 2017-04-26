be617898fb97f8266eca8de21973b8f55ab4ffa1
Claude Pageau <pageauc@gmail.com>
2017-04-26 06:41:05 -0400

    Minor Edit to fix code formatting

--- motion-track.py
+++ motion-track.py
@@ -123,42 +123,43 @@ class PiVideoStream:
     def stop(self):
         # indicate that the thread should be stopped
         self.stopped = True
-
+        
+#-----------------------------------------------------------------------------------------------
 class WebcamVideoStream:
-        def __init__(self, src=0):
-                # initialize the video camera stream and read the first frame
-                # from the stream
-                self.stream = cv2.VideoCapture(src)
-                (self.grabbed, self.frame) = self.stream.read()
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
 
-                # initialize the variable used to indicate if the thread should
-                # be stopped
-                self.stopped = False
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
 
-        def start(self):
-                # start the thread to read frames from the video stream
-                t = Thread(target=self.update, args=())
-                t.daemon = True
-                t.start()
-                return self
+    def start(self):
+        # start the thread to read frames from the video stream
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
 
-        def update(self):
-                # keep looping infinitely until the thread is stopped
-                while True:
-                        # if the thread indicator variable is set, stop the thread
-                        if self.stopped:
-                                return
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                    return
 
-                        # otherwise, read the next frame from the stream
-                        (self.grabbed, self.frame) = self.stream.read()
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
 
-        def read(self):
-                # return the frame most recently read
-                return self.frame
+    def read(self):
+        # return the frame most recently read
+        return self.frame
 
-        def stop(self):
-                # indicate that the thread should be stopped
-                self.stopped = True
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
 
 #-----------------------------------------------------------------------------------------------  
 def show_FPS(start_time,frame_count):
