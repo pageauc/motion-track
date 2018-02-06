cf7a061b96f73260165f794fa89bed0e078f59d3
Claude Pageau <pageauc@gmail.com>
2018-02-06 09:25:09 -0600

    Minor settings changes

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ show_fps = False    # Show Frames per second
 diff_window_on = False    # Show OpenCV image difference window
 thresh_window_on = False  # Show OpenCV image Threshold window
 SHOW_CIRCLE = True  # True= show circle False= show rectancle on biggest motion
-CIRCLE_SIZE = 5     # diameter of circle for SHOW_CIRCLE
+CIRCLE_SIZE = 3     # diameter of circle for SHOW_CIRCLE
 LINE_THICKNESS = 2  # thickness of bounding line in pixels
 WINDOW_BIGGER = 2   # Resize multiplier for Movement Status Window
                     # if gui_window_on=True then makes opencv window bigger
@@ -25,14 +25,14 @@ WEBCAM = False        # default = False False=PiCamera True=USB WebCamera
 WEBCAM_SRC = 0        # default = 0   USB opencv connection number
 WEBCAM_WIDTH = 320    # default = 320 USB Webcam Image width
 WEBCAM_HEIGHT = 240   # default = 240 USB Webcam Image height
-WEBCAM_HFLIP = False  # default = False USB Webcam flip image horizontally
+WEBCAM_HFLIP = True   # default = False USB Webcam flip image horizontally
 WEBCAM_VFLIP = False  # default = False USB Webcam flip image vertically
 
 # Pi Camera Settings
 CAMERA_WIDTH = 320    # default = 320 PiCamera image width can be greater if quad core RPI
 CAMERA_HEIGHT = 240   # default = 240 PiCamera image height
-CAMERA_HFLIP = False  # True=flip camera image horizontally
-CAMERA_VFLIP = False  # True=flip camera image vertically
+CAMERA_HFLIP = True   # True=flip camera image horizontally
+CAMERA_VFLIP = True   # True=flip camera image vertically
 CAMERA_ROTATION = 0   # Rotate camera image valid values 0, 90, 180, 270
 CAMERA_FRAMERATE = 25 # default = 25 lower for USB Web Cam. Try different settings
 FRAME_COUNTER = 1000  # used when show_fps=True  Sets frequency of display
