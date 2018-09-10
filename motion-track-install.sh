be1799d366e0ff17df58d09160a49ded97e52957
Claude Pageau <pageauc@gmail.com>
2018-09-09 20:20:47 -0400

    release 1.3 Add pythn3-picamera install

--- motion-track-install.sh
+++ motion-track-install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Convenient motion-track motion-track-install.sh script written by Claude Pageau 1-Jul-2016
-ver="1.2"
+ver="1.3"
 APP_DIR='motion-track-demo'  # Default folder install location
 
 cd ~
 if [ -d "$APP_DIR" ] ; then
   STATUS="Upgrade"
   echo "Upgrade motion-track files"
-else  
+else
   echo "New motion-track Install"
   STATUS="New Install"
   mkdir -p $APP_DIR
   echo "$APP_DIR Folder Created"
-fi 
+fi
 
 cd $APP_DIR
-INSTALL_PATH=$( pwd )   
+INSTALL_PATH=$( pwd )
 
 # Remember where this script was launched from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -28,13 +28,13 @@ echo "1 - Downloading GitHub Repo files to $INSTALL_PATH"
 wget -O motion-track-install.sh -q --show-progress https://raw.github.com/pageauc/motion-track/master/motion-track-install.sh
 if [ $? -ne 0 ] ;  then
   wget -O motion-track-install.sh https://raw.github.com/pageauc/motion-track/master/motion-track-install.sh
-  wget -O motion-track.py https://raw.github.com/pageauc/motion-track/master/motion-track.py 
-  wget -O config.py https://raw.github.com/pageauc/motion-track/master/config.py  
-  wget -O Readme.md https://raw.github.com/pageauc/motion-track/master/Readme.md  
+  wget -O motion-track.py https://raw.github.com/pageauc/motion-track/master/motion-track.py
+  wget -O config.py https://raw.github.com/pageauc/motion-track/master/config.py
+  wget -O Readme.md https://raw.github.com/pageauc/motion-track/master/Readme.md
 else
   wget -O motion-track.py -q --show-progress https://raw.github.com/pageauc/motion-track/master/motion-track.py
-  wget -O config.py -q --show-progress https://raw.github.com/pageauc/motion-track/master/config.py    
-  wget -O Readme.md -q --show-progress  https://raw.github.com/pageauc/motion-track/master/Readme.md  
+  wget -O config.py -q --show-progress https://raw.github.com/pageauc/motion-track/master/config.py
+  wget -O Readme.md -q --show-progress  https://raw.github.com/pageauc/motion-track/master/Readme.md
 fi
 echo "Done Download"
 echo "------------------------------------------------"
@@ -49,7 +49,7 @@ NOW="$( date +%d-%m-%y )"
 LAST="$( date -r /var/lib/dpkg/info +%d-%m-%y )"
 if [ "$NOW" == "$LAST" ] ; then
   echo "4 Raspbian System is Up To Date"
-  echo ""  
+  echo ""
 else
   echo ""
   echo "3 - Performing Raspbian System Update"
@@ -64,11 +64,11 @@ else
   echo ""
   sudo apt-get -y upgrade
   echo "Done upgrade"
-fi  
+fi
 echo "------------------------------------------------"
 echo ""
 echo "5 - Installing motion-track Dependencies"
-sudo apt-get install -y python-opencv python-picamera
+sudo apt-get install -y python-opencv python-picamera python3-picamera
 echo "Done Dependencies"
 cd $DIR
 # Check if motion-track-install.sh was launched from motion-track folder
@@ -85,7 +85,7 @@ echo ""
 echo "1. Reboot RPI if there are significant Raspbian system updates"
 echo "2. Raspberry pi needs a monitor/TV attached to display game window"
 echo "3. Run motion-track.py with the Raspbian Desktop GUI running"
-echo "4. To start open file manager or a Terminal session then change to" 
+echo "4. To start open file manager or a Terminal session then change to"
 echo "   motion-track folder and launch per commands below"
 echo ""
 echo "   cd ~/motion-track-demo"
